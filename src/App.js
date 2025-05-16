624fe3a77d8a04a8033e7222e06c1ca9241541fe
rohandhiman222 <53372822+rohandhiman222@users.noreply.github.com>
2025-05-16 06:12:29 +0530

    cahnged updated

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
         <div className='App'>
           <header className='App-header'>
             <img src={logo} className='App-logo' alt='logo' />
-            <h1 className='App-title'>Simple React App updated</h1>
+            <h1 className='App-title'>Simple React App updated agian</h1>
           </header>
           <Switch>
             <Route
